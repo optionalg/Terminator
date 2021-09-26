bda61741a0ea7070a940e29cad07200a70a233eb
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-09-26 17:46:20 +0400

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,21 +1,20 @@
-sleep 2
-echo "\033[1;34m[*] \033[0mInitiliazing..."
+echo -e "\033[1;34m[*] \033[0mInitiliazing..."
 apt clean > /dev/null 2>&1
 apt autoremove -y > /dev/null 2>&1
 clear
-echo "\033[1;34m[*] \033[0mInstalling Requirements..."
+echo -e "\033[1;34m[*] \033[0mInstalling Requirements..."
 apt install ruby perl metasploit-framework xterm zenity python python3-pip gem git -y > /dev/null 2>&1
 pip3 install colorama scapy pysocks imports future paramiko flask > /dev/null 2>&1
 pip3 install bs4 > /dev/null 2>&1
-echo "\033[1;34m[*] \033[0mCopying Additional Files > /usr/share..."
+echo -e "\033[1;34m[*] \033[0mCopying Additional Files > /usr/share..."
 python3 cp/cp.py
 echo "\033[1;34m[*] \033[0mCopying tmconsole > /usr/bin..."
 cp -r bin/tmconsole/tmconsole /usr/bin > /dev/null 2>&1
-chmod +x /usr/bin/tmconsole > /dev/null 2>&1
-echo "\033[1;34m[*] \033[0mCleaning Up..."
-echo "\033[1;34m[*] \033[0mSetup Finished!"
+chmod +x /usr/bin/tmconsole
+echo -e "\033[1;34m[*] \033[0mCleaning Up..."
+echo -e "\033[1;34m[*] \033[0mSetup Finished!"
 echo
-echo "\033[1;32m[+] \033[0mTo Start Terminator Just Type '\033[1;32mtmconsole\033[0m' In Terminal!"
+echo -e "\033[1;32m[+] \033[0mTo Start Terminator Just Type '\033[1;32mtmconsole\033[0m' In Terminal!"
 #\033[1;77m[i] \033[0m
 #\033[1;77m[?] \033[0m
 #\033[1;32m[+] \033[0m
--- uninstall.sh
+++ uninstall.sh
@@ -1,2 +1,2 @@
 bash /usr/share/Terminator/bin/bin.sh
-echo "\033[1;32m[+] \033[0mTerminator Framework Has Uninstalled From Your System"
+echo -e "\033[1;32m[+] \033[0mTerminator Framework Has Uninstalled From Your System"
