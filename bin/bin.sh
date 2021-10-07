c6a28bc745f41fb0203ced76006f828f6101fc24
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-10-07 17:00:52 +0400

    Add files via upload

--- bin/bin.sh
+++ bin/bin.sh
@@ -1,3 +1,4 @@
-rm -rf /usr/bin/tmconsole
-rm -rf /usr/share/Terminator
-rm -rf /root/.tmf
+rm -rf /usr/bin/tmconsole > /dev/null 2>&1
+rm -rf /usr/share/Terminator > /dev/null 2>&1
+rm -rf /root/.tmf > /dev/null 2>&1
+rm -rf /usr/var/tmf-meta-inf > /dev/null 2>&1
