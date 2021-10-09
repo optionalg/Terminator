8ec61fa0e85ab71483b8dfa147b4cfafd7a43624
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-10-09 12:31:02 +0400

    Add files via upload

--- bin/version/ver.py
+++ bin/version/ver.py
@@ -8,7 +8,9 @@ modules = "/usr/share/Terminator/modules"
 try:
     if os.path.exists("/usr/share/Terminator/lib/plugins/meta/extractor.py"):
         os.system('python3 /usr/share/Terminator/lib/plugins/meta/extractor.py '+modules+' modules')
-        print(Fore.YELLOW+'[+]'+Fore.RESET+' Extracting Updated Files...')
+        print(Fore.BLUE+'[*]'+Fore.RESET+' Extracting Updated Files...')
+        time.sleep(0.5)
+        print(Fore.YELLOW+'[+]'+Fore.RESET+' Update Completed.')
     else:
         print(Fore.RED+'[-]'+Fore.RESET+' Extractor Not Found!')
         sys.exit()
