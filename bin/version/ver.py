e3d08fc93bab745f961bff8ebc767344ed1bfa88
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-10-16 16:03:13 +0400

    Add files via upload

--- bin/version/ver.py
+++ bin/version/ver.py
@@ -3,11 +3,16 @@ import colorama
 from colorama import Fore
 import sys
 import time
+import random
 colorama.init()
 modules = "/usr/share/Terminator/modules"
+core = "/usr/share/Terminator/core"
+boot = "/usr/share/Terminator/core/common/boot"
+all = ['/usr/share/Terminator/modules', '/usr/share/Terminator/core', '/usr/share/Terminator/core/common/boot']
+types = ['modules', 'core', 'boot']
 try:
     if os.path.exists("/usr/share/Terminator/lib/plugins/meta/extractor.py"):
-        os.system('python3 /usr/share/Terminator/lib/plugins/meta/extractor.py '+modules+' modules')
+        os.system('python3 /usr/share/Terminator/lib/plugins/meta/extractor.py '+random.choice(all)+' '+random.choice(types))
         print(Fore.BLUE+'[*]'+Fore.RESET+' Extracting Updated Files...')
         time.sleep(0.7)
         print(Fore.YELLOW+'[+]'+Fore.RESET+' Update Completed.')
