6719c989e751f7a12bad458e99f33e05374581cf
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-10-24 10:26:56 +0400

    Add files via upload

--- bin/version/ver.py
+++ bin/version/ver.py
@@ -14,7 +14,9 @@ except:
     pass
 try:
     if os.path.exists("/usr/share/Terminator/lib/plugins/meta/extractor.py"):
-        print(Fore.BLUE+'[*]'+Fore.RESET+' Reloading Plugins...')
+        print(Fore.BLUE+'[*]'+Fore.RESET+' Extracting Files...')
+        time.sleep(0.1)
+        print(Fore.BLUE+'[*]'+Fore.RESET+' Reloading Plugins....')
         time.sleep(0.2)
         os.system('python3 /usr/share/Terminator/lib/plugins/meta/extractor.py')
         print(Fore.YELLOW+'[+]'+Fore.RESET+' Please Restart Terminator To Setup Changes!')
