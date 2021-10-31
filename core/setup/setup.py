77991a58e2c0160355d3435297c354ab0b622462
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-10-31 23:11:02 +0400

    Add files via upload

--- core/setup/setup.py
+++ core/setup/setup.py
@@ -50,13 +50,13 @@ def check():
         except:
             pass
         time.sleep(0.1)
-        sys.stdout.write(f'\r[{now}] Checking Dependiences...                          '+Style.BRIGHT+'STATUS:'+Style.RESET_ALL+' [Working]')
+        sys.stdout.write(f'\r[{now}] Checking Dependiences...                      '+Style.BRIGHT+'STATUS:'+Style.RESET_ALL+' [Working]')
         time.sleep(0.4)
         try:
             if os.path.exists("/usr/bin/python") and os.path.exists("/usr/bin/python3") and os.path.exists("/usr/bin/msfvenom") and os.path.exists("/usr/bin/msfconsole"):
-                sys.stdout.write(f'\r[{now}] Checking Dependiences...                             '+Style.BRIGHT+'STATUS:'+Style.RESET_ALL+' ['+Fore.GREEN+'OK'+Fore.RESET+']\n')
+                sys.stdout.write(f'\r[{now}] Checking Dependiences...                          '+Style.BRIGHT+'STATUS:'+Style.RESET_ALL+' ['+Fore.GREEN+'OK'+Fore.RESET+']\n')
             else:
-                sys.stdout.write(f'\r[{now}] Checking Dependiences...                             '+Style.BRIGHT+'STATUS:'+Style.RESET_ALL+' ['+Fore.BLUE+'Installing...'+Fore.RESET+']\n')
+                sys.stdout.write(f'\r[{now}] Checking Dependiences...                          '+Style.BRIGHT+'STATUS:'+Style.RESET_ALL+' ['+Fore.BLUE+'Installing...'+Fore.RESET+']\n')
                 os.system('bash core/setup/install.sh')
         except:
             pass
