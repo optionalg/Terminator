348cbce22058922683a02f41e3393c4c82ecf3bf
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-11-10 21:04:56 +0300

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
-bash /usr/share/Terminator/bin/bin.sh
-bash bin/bin.sh
+bash /usr/share/Terminator/bin/bin.sh > /dev/null 2>&1
+bash bin/bin.sh > /dev/null 2>&1
 echo -e "\033[1;32m[+] \033[0mTerminator Framework Successfully UnInstalled From Your System!"
