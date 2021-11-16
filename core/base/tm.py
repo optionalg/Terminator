af3f14bc05df5f757e4c34e39395fc655850d185
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-11-16 12:44:53 +0400

    Update tm.py

--- core/base/tm.py
+++ core/base/tm.py
@@ -17,7 +17,7 @@ try:
     with open("/usr/share/Terminator/core/logs/logs.log", "r") as error:
         for line in error:
             if 'FATAL' in line:
-                print(Fore.RED+"\n[-]"+Fore.RESET+" Got Error In Logs: "+line)
+                print(Fore.RED+"[-]"+Fore.RESET+" Got Error In Logs: "+line)
 except:
     pass
 try:
