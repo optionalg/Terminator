df9e7c23ae915b37c0200d1bb97f5b4ffd7068cb
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-11-16 10:57:08 +0300

    Add files via upload

--- core/common/boot/boot_helpers/dsms.py
+++ core/common/boot/boot_helpers/dsms.py
@@ -230,10 +230,10 @@ def animate():
                 l = os.listdir("/usr/share/Terminator/modules")
                 for i in l:
                     a = os.listdir("/usr/share/Terminator/modules/"+i)
-                    write_log = open("/usr/share/Terminator/core/logs/logs.log", "a")
                     for load in a:
-                        write_log.write(f"\n[{timerun}] NOTE: Loading Additional Module {load}")
-                        write_log.close()
+                        with open("/usr/share/Terminator/core/logs/logs.log", "a") as write_log:
+                            write_log.write(f"\n[{timerun}] NOTE: Loading Additional Module {load}")
+                            write_log.close()
             else:
                 with open("/usr/share/Terminator/core/logs/logs.log", "a") as fail:
                     fail.write(f"\n[{timerun}] FATAL: Unable To Load Modules Database")
