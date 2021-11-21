a5a22fc7c70da17973e15aa5ce97b4dd6074b9c4
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-11-21 14:44:58 +0300

    Add files via upload

--- core/base/tm.py
+++ core/base/tm.py
@@ -14,11 +14,11 @@ colorama.init()
 user = getpass.getuser()
 try:
     print('')
-    error_got = ""
+    error_got = []
     with open("/usr/share/Terminator/core/logs/logs.log", "r") as error:
         for line in error:
             if 'FATAL' in line:
-                error_got = line
+                error_got.append(line)
                 print(Fore.RED+"[-]"+Fore.RESET+" Got Error In Logs: "+line)
 except:
     pass
@@ -48,19 +48,13 @@ def removeses():
     except:
         pass
 try:
-    with open("/usr/share/Terminator/bin/version/ver.yaml", "r") as yr:
-        ver = yr.read()
-        yr.close()
-except:
-    pass
-try:
     if os.path.exists("/usr/share/Terminator/lib/plugins/update"):
         updater = Fore.GREEN+"OK"+Fore.RESET
     else:
         updater = Fore.RED+"FATAL"+Fore.RESET
 except:
     pass
-version = "1.8.4.2"+Fore.LIGHTYELLOW_EX+"#dev"
+version = "1.8.4.3"+Fore.LIGHTYELLOW_EX+"#dev"
 commands = '''
 Global Commands
 ===============
