4a4201b62a2372de5a99c8f20d0b0f1b7e96004f
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-11-23 16:32:12 +0300

    Add files via upload

---
+++ lib/plugins/update/install.py
@@ -0,0 +1,19 @@
+import os
+import time
+import subprocess
+import sys
+import getpass
+import colorama
+from colorama import Fore
+colorama.init()
+def check():
+    try:
+        if os.path.exists("/usr/share/Terminator/lib/plugins/update/update.sh"):
+            os.system('bash /usr/share/Terminator/lib/plugins/update/update.sh')
+        else:
+            print(Fore.RED+'[-]'+Fore.RESET+' Update Checker File Corrupted Or Removed!')
+    except:
+        pass
+
+check()
+sys.exit()
---
+++ lib/plugins/update/update.py
@@ -0,0 +1,31 @@
+import os
+import sys
+import time
+import colorama
+from colorama import Fore
+def db():
+    print(Fore.BLUE+'[*]'+Fore.RESET+' Updating Terminator Framework...')
+    url = "https://github.com/G00Dway/Terminator"
+    try:
+        if os.path.exists("/usr/var/tmf-meta-inf/Terminator-old"):
+            os.system('rm -rf /usr/var/tmf-meta-inf/Terminator-old > /dev/null 2>&1')
+        else:
+            pass
+        if os.path.exists("/usr/share/Terminator"):
+            os.system('mv /usr/share/Terminator /usr/var/tmf-meta-inf/Terminator-old > /dev/null 2>&1')
+            os.system("git clone "+url+" /usr/share/Terminator > /dev/null 2>&1")
+            if os.path.exists("/usr/share/Terminator"):
+                os.system('rm -rf /usr/bin/tmconsole > /dev/null 2>&1')
+                os.system('chmod +x /usr/share/Terminator/bin/tmconsole/tmconsole > /dev/null 2>&1')
+                os.system('cp -r /usr/share/Terminator/bin/tmconsole/tmconsole /usr/bin > /dev/null 2>&1')
+                os.system('python3 /usr/share/Terminator/bin/version/ver.py')
+            else:
+                print(Fore.RED+'[-]'+Fore.RESET+' Update Failed, Using Old Database Instead New')
+                os.system('mv /usr/var/tmf-meta-inf/Terminator-old /usr/share/Terminator > /dev/null 2>&1')
+        else:
+            print(Fore.RED+'[-]'+Fore.RESET+' Unable To Update, Database Removed Or Corrupted!')
+    except:
+        pass
+
+
+db()
