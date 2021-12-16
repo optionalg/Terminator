699a6cf8f2d0cd843328a2f7ebf1cdf141711052
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2021-12-16 16:12:14 +0400

    Add files via upload

--- modules/other/nmap.py
+++ modules/other/nmap.py
@@ -16,15 +16,6 @@ port_min = 0
 port_max = 65535
 open_ports = []
 while True:
-    try:
-        ip_address_obj = ipaddress.ip_address(ip_add_entered)
-        print(Fore.YELLOW+"[+]"+Fore.RESET+" You Entered a Valid IP Address.")
-        break
-    except:
-        print(Fore.RED+"[-]"+Fore.RESET+" You Entered An Invalid IP Address")
-    
-
-while True:
     port_range_valid = port_range_pattern.search(port_range.replace(" ",""))
     if port_range_valid:
         port_min = int(port_range_valid.group(1))
