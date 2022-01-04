b0b309daff62761209bf93848f36b631a0fc2afe
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2022-01-04 14:48:16 +0400

    Add files via upload

--- lib/plugins/meta/extractor.py
+++ lib/plugins/meta/extractor.py
@@ -11,23 +11,16 @@ try:
     os.mkdir('/usr/share/Terminator/core/logs/cache_meta')
 except:
     pass
-def extr():
-    try:
-        os.system('mv /usr/bin/tmconsole /usr/share/Terminator/core/logs/cache_meta > /dev/null 2>&1')
-        os.system('chmod +x /usr/share/Terminator/bin/tmconsole/tmconsole > /dev/null 2>&1')
-        os.system('cp -r /usr/share/Terminator/bin/tmconsole/tmconsole /usr/bin > /dev/null 2>&1')
-        os.system('python3 /usr/share/Terminator/bin/version/ver.py')
-    except:
-        pass
-
-
-def install():
-    try:
-        os.system('cp -r /usr/share/Terminator /usr/share/Terminator/core/logs/meta > /dev/null 2>&1')
-        os.system('mv /usr/share/Terminator/core/logs/meta/Terminator /usr/share/Terminator/core/logs/meta/Terminator.zip > /dev/null 2>&1')
-        extr()
-    except:
-        pass
-
-install()
+try:
+    os.system('cp -r /usr/share/Terminator /usr/share/Terminator/core/logs/meta > /dev/null 2>&1')
+    os.system('mv /usr/share/Terminator/core/logs/meta/Terminator /usr/share/Terminator/core/logs/meta/Terminator-new > /dev/null 2>&1')
+except:
+    pass
+try:
+    os.system('mv /usr/bin/tmconsole /usr/share/Terminator/core/logs/cache_meta > /dev/null 2>&1')
+    os.system('chmod +x /usr/share/Terminator/bin/tmconsole/tmconsole > /dev/null 2>&1')
+    os.system('cp -r /usr/share/Terminator/bin/tmconsole/tmconsole /usr/bin > /dev/null 2>&1')
+    os.system('python3 /usr/share/Terminator/bin/version/ver.py')
+except:
+    pass
 sys.exit()
