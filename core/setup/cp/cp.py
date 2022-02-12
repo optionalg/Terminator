bc7533573cb05f88c663d4d426b203c211af1dc6
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2022-02-12 17:01:28 +0400

    Add files via upload

--- core/setup/cp/cp.py
+++ core/setup/cp/cp.py
@@ -9,7 +9,7 @@ user = getpass.getuser()
 url = "https://github.com/G00Dway/Terminator"
 try:
     if os.path.exists("/usr/share/Terminator"):
-        print(Fore.BLUE+'[*]'+Fore.RESET+' Removing Previous Database...')
+        print(Fore.BLUE+'\n[*]'+Fore.RESET+' Removing Previous Database...')
         os.system('rm -rf /usr/share/Terminator > /dev/null 2>&1')
         os.system('rm -rf /usr/var/tmf-meta-inf > /dev/null 2>&1')
         os.system('rm -rf /root/.tmf > /dev/null 2>&1')
