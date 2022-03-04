f0f6883d781d306d7f171f283751bd1555d8566d
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2022-03-04 21:59:01 +0400

    Update update.sh

--- lib/plugins/update/update.sh
+++ lib/plugins/update/update.sh
@@ -1,5 +1,5 @@
 cd /usr/share/Terminator
-echo -e "\033[1;34m[*] \033[0mChecking For Latest Updates..."
+echo -e "\033[1;34m[*] \033[0mChecking For Updates..."
 BRANCH="main"
 LAST_UPDATE=`git show --no-notes --format=format:"%H" $BRANCH | head -n 1`
 LAST_COMMIT=`git show --no-notes --format=format:"%H" origin/$BRANCH | head -n 1`
